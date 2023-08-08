86f3e2d8befc9171b993df5b2f9333c530147438
mirtrudmay-game <mirtrudmay.game@gmail.ru>
2023-08-08 19:19:18 +0300

    fix: navigation to Program not working

--- src/containers/4_Program/Program.tsx
+++ src/containers/4_Program/Program.tsx
@@ -7,9 +7,9 @@ import waveR_xl from '@src/assets/blocks/wave_right.svg'
 
 export function Program() {
   return (
-    <section className={styles.page}>
-      <img src={waveL_xl} className={styles.wave_l} alt={""}/>
-      <img src={waveR_xl} className={styles.wave_r} alt={""}/>
+    <section className={styles.page} id='program'>
+      <img src={waveL_xl} className={styles.wave_l} alt={''} />
+      <img src={waveR_xl} className={styles.wave_r} alt={''} />
 
       <div className={styles.container}>
         <Title title={'программа мероприятия'} subtitle={'27.05.2023'} />
